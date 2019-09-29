6a0b5afe457f6001dcc31fa38b76aeee2b94ac97
Jake Wheeler <jakewhlr@gmail.com>
2019-09-29 00:25:28 -0700

    Added most models to API

--- turkeybowl_site/turkeybowl_site/urls.py
+++ turkeybowl_site/turkeybowl_site/urls.py
@@ -13,9 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from tastypie.api import Api
+
+from turkeybowl.api.resources import *
+
+v1_api = Api(api_name='v1')
+v1_api.register(TeamResource())
+v1_api.register(TeamPerformanceResource())
+v1_api.register(GameResource())
+v1_api.register(PlayerResource())
+v1_api.register(TeamStatResource())
+v1_api.register(PlayerStatResource())
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^api/', include(v1_api.urls)),
 ]
