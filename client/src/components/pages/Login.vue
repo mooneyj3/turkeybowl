8e73d9360441e3fedd06b34fa5d56b7c9c637525
Jonny Mooneyham <mooneyj3@wwu.edu>
2019-09-29 10:57:04 -0700

    Added client component and updated .gitignore

---
+++ client/src/components/pages/Login.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="login">
+        <h1>Login Page</h1>
+        <p>Login page will go here</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Login"
+    }
+</script>
+
+<style scoped>
+
+</style>
