786df1e2d13ae78c0c5e0acf908581bfe23f9514
Jonny Mooneyham <jonathan.mooneyham@gmail.com>
2019-10-02 17:22:53 -0700

    [client] added view team and other slight modifications to UI

--- client/src/components/NavigationDrawer/NavigationDrawer.vue
+++ client/src/components/NavigationDrawer/NavigationDrawer.vue
@@ -3,55 +3,49 @@
         <v-navigation-drawer v-model="drawer" app right>
             <v-list dense>
                 <!--Home-->
-                <router-link to="/">
-                    <v-list-item>
-                        <v-list-item-action>
-                            <v-icon>home</v-icon>
-                        </v-list-item-action>
-                        <v-list-item-content>
-                            <v-list-item-title>Home</v-list-item-title>
-                        </v-list-item-content>
-                    </v-list-item>
-                </router-link>
+                <!--<router-link to="/">-->
+                <v-list-item @click="goToPage('/')">
+                    <v-list-item-action>
+                        <v-icon>home</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title>Home</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+                <!--</router-link>-->
 
                 <!--My Team-->
-                <router-link to="/editteam" v-if="isLoggedIn">
-                    <v-list-item>
-                        <v-list-item-action>
-                            <v-icon>people</v-icon>
-                        </v-list-item-action>
-                        <v-list-item-content>
-                            <v-list-item-title>Edit My Team</v-list-item-title>
-                        </v-list-item-content>
-                    </v-list-item>
-                </router-link>
+                <v-list-item @click="goToPage('/team')" v-if="isLoggedIn">
+                    <v-list-item-action>
+                        <v-icon>people</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title>My Team</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
 
                 <!--My Profile-->
-                <router-link to="/editprofile" v-if="isLoggedIn">
-                    <v-list-item>
-                        <v-list-item-action>
-                            <v-icon>person</v-icon>
-                        </v-list-item-action>
-                        <v-list-item-content>
-                            <v-list-item-title>My Profile</v-list-item-title>
-                        </v-list-item-content>
-                    </v-list-item>
-                </router-link>
+                <v-list-item @click="goToPage('/editprofile')" v-if="isLoggedIn">
+                    <v-list-item-action>
+                        <v-icon>person</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title>My Profile</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
 
                 <!--login-->
-                <router-link to="/login" v-if="!isLoggedIn">
-                    <v-list-item>
-                        <v-list-item-action>
-                            <v-icon>person</v-icon>
-                        </v-list-item-action>
-                        <v-list-item-content>
-                            <v-list-item-title>Login</v-list-item-title>
-                        </v-list-item-content>
-                    </v-list-item>
-                </router-link>
+                <v-list-item @click="goToPage('/login')" v-if="!isLoggedIn">
+                    <v-list-item-action>
+                        <v-icon>person</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title>Login</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
 
                 <!-- logout -->
-                <div v-if="isLoggedIn" >
+                <div v-if="isLoggedIn">
                     <v-list-item v-on:click="logout">
                         <v-list-item-action>
                             <v-icon>person</v-icon>
@@ -83,13 +77,20 @@
                 this.$store.dispatch('logout')
                     .then(() => this.$router.push('/'))
             },
+            goToPage: function (destination) {
+                this.$router.push(destination).resolve();
+            }
         },
         computed: {
-            isLoggedIn() {return this.$store.getters.isLoggedIn}
+            isLoggedIn() {
+                return this.$store.getters.isLoggedIn
+            }
         },
     }
 </script>
 
 <style scoped>
-    a { text-decoration: none; }
+    a {
+        text-decoration: none;
+    }
 </style>
---
+++ client/src/components/Team/ViewTeam.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>viewteam</div>
+</template>
+
+<script>
+    export default {
+        name: "ViewTeam",
+        data() {
+            return {
+                team: {
+                    QB: {
+                        name: 'Matthew Stafford',
+                        team: 'DET',
+                    },
+                    RB1: {
+                        name: 'Kerryon Johnson',
+                        team: 'DET',
+                    },
+                    RB2: {
+                        name: 'J.D. McKissic',
+                        team: 'DET',
+                    },
+                    WR1: {
+                        name: 'Marvin Jones Jr.',
+                        team: 'DET'
+                    },
+                    WR2: {
+                        name: 'Travis Fulgham',
+                        team: 'DET'
+                    },
+                    TE: {
+                        name: 'Jesse James',
+                        team: 'DET'
+                    },
+                    K: {
+                        name: 'Matt Prater',
+                        team: 'DET'
+                    },
+                    DST: {
+                        name: 'Lions',
+                        team: 'DET'
+                    },
+                },
+
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
