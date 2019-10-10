16e94ec7d02869f8d14a14ff18e5681aed15a99f
Jonny Mooneyham <jonathan.mooneyham@gmail.com>
2019-10-10 14:09:42 -0700

    testing django_rest_services

--- django/turkeybowl_site/turkeybowl_site/settings.py
+++ django/turkeybowl_site/turkeybowl_site/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'nfl_stats.apps.NflStatsConfig',
     'tastypie',
     'polymorphic',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ],
+
+    # PAGINATION: allows you to control how many objects per page are returned.
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
--- django/turkeybowl_site/turkeybowl_site/urls.py
+++ django/turkeybowl_site/turkeybowl_site/urls.py
@@ -15,18 +15,28 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from tastypie.api import Api
 
 from nfl_stats.api.resources import *
 
-v1_api = Api(api_name='v1')
-v1_api.register(TeamResource())
-v1_api.register(PlayerResource())
-v1_api.register(GameResource())
-v1_api.register(PlayerResource())
-v1_api.register(StatResource())
+# Django rest framework
+from django.urls import path
+from rest_framework import routers
+from rest_api import views
+
+# v1_api = Api(api_name='v1')
+# v1_api.register(TeamResource())
+# v1_api.register(PlayerResource())
+# v1_api.register(GameResource())
+# v1_api.register(PlayerResource())
+# v1_api.register(StatResource())
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^api/', include(v1_api.urls)),
+    # url(r'^api/', include(v1_api.urls)),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
